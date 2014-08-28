de7a5a65451b4000422e35c16ffefea411b80c97
Anuj Garg <anujgarg1208@gmail.com>
2014-08-28 16:58:05 -0400

    Commit for first push

---
+++ Bouncing Ball/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Bouncing_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bottomright = new System.Windows.Forms.Timer(this.components);
+            this.topright = new System.Windows.Forms.Timer(this.components);
+            this.topleft = new System.Windows.Forms.Timer(this.components);
+            this.bottomleft = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.levelcounter = new System.Windows.Forms.Timer(this.components);
+            this.highscoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(41, 182);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bottomright
+            // 
+            this.bottomright.Interval = 10;
+            this.bottomright.Tick += new System.EventHandler(this.bottomright_Tick);
+            // 
+            // topright
+            // 
+            this.topright.Interval = 10;
+            this.topright.Tick += new System.EventHandler(this.topright_Tick);
+            // 
+            // topleft
+            // 
+            this.topleft.Interval = 10;
+            this.topleft.Tick += new System.EventHandler(this.topleft_Tick);
+            // 
+            // bottomleft
+            // 
+            this.bottomleft.Interval = 10;
+            this.bottomleft.Tick += new System.EventHandler(this.bottomleft_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(76, 253);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(15, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(222, 145);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(15, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.highscoresToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Developed By Anuj";
+            // 
+            // levelcounter
+            // 
+            this.levelcounter.Interval = 10000;
+            this.levelcounter.Tick += new System.EventHandler(this.levelcounter_Tick);
+            // 
+            // highscoresToolStripMenuItem
+            // 
+            this.highscoresToolStripMenuItem.Name = "highscoresToolStripMenuItem";
+            this.highscoresToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.highscoresToolStripMenuItem.Text = "Highscores";
+            this.highscoresToolStripMenuItem.Click += new System.EventHandler(this.highscoresToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Pong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer bottomright;
+        private System.Windows.Forms.Timer topright;
+        private System.Windows.Forms.Timer topleft;
+        private System.Windows.Forms.Timer bottomleft;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer levelcounter;
+        private System.Windows.Forms.ToolStripMenuItem highscoresToolStripMenuItem;
+    }
+}
+
---
+++ Bouncing Ball/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Bouncing_Ball
+{
+    public partial class Form1 : Form
+    {
+        int speed=5,bmargin=88,tmargin=25,level=1;        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Point p = new Point(); //for ball
+        Point q = new Point(); //for ball following slider
+        Point r = new Point(); //for mouse following slider
+        int cnt=0;        
+        private void Form1_Load(object sender, EventArgs e)
+        {                      
+            r.X = 5;
+            r.Y = MousePosition.Y - this.Location.Y - 70;
+            pictureBox3.Location = r;
+            q.X = this.Size.Width - 35;            
+            p.Y = pictureBox1.Location.Y;
+            q.Y = p.Y;
+            pictureBox2.Location = q;
+            p.X = pictureBox1.Location.X;                       
+        }      
+        
+        private void bottomright_Tick(object sender, EventArgs e)
+        {            
+            if (p.X + bmargin >= this.Size.Width)
+            {
+                bottomright.Enabled = false;
+                bottomleft.Enabled = true;
+            }
+            else if (p.Y + bmargin >= this.Size.Height)
+            {
+                bottomright.Enabled = false;
+                topright.Enabled = true;
+            }
+            else
+            {
+                r.Y = MousePosition.Y - this.Location.Y - 70;
+                pictureBox3.Location = r;             
+                p.Y += speed;                
+                p.X += speed;                
+                pictureBox1.Location = p;
+                q.Y = p.Y;
+                pictureBox2.Location = q;
+            }
+        }
+
+        private void topright_Tick(object sender, EventArgs e)
+        {
+            if (p.X + bmargin >= this.Size.Width)
+            {
+                topright.Enabled = false;
+                topleft.Enabled = true;
+            }
+            else if (p.Y <= tmargin)
+            {
+
+                topright.Enabled = false;
+                bottomright.Enabled = true;
+            }                
+            else
+            {                
+                r.Y = MousePosition.Y - this.Location.Y - 70;
+                pictureBox3.Location = r;             
+                p.Y -= speed;
+                p.X += speed;
+                pictureBox1.Location = p;
+                q.Y = p.Y;
+                pictureBox2.Location = q;
+            }
+        }
+        
+        private void topleft_Tick(object sender, EventArgs e)
+        {
+            if (p.X <= tmargin)
+            {
+                if (r.Y <= p.Y + 25 && r.Y >= p.Y - 25)
+                {
+                    topleft.Enabled = false;
+                    topright.Enabled = true;
+                }
+                else
+                {
+                    cnt++;
+                }
+            }
+            else if (p.Y <= tmargin)
+            {
+                topleft.Enabled = false;
+                bottomleft.Enabled = true;
+            }
+            if (cnt <= 10)
+            {
+                r.Y = MousePosition.Y - this.Location.Y - 70;
+                pictureBox3.Location = r;           
+                p.Y -= speed;
+                p.X -= speed;
+                pictureBox1.Location = p;
+                q.Y = p.Y;
+                pictureBox2.Location = q;
+            }
+            else
+            {                                                    
+                topleft.Enabled = false;
+                levelcounter.Enabled = false;
+                FileStream fs = new FileStream("highscores.txt", FileMode.Append, FileAccess.Write, FileShare.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine("Level: " + level);
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("Game Over");
+                fileToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void bottomleft_Tick(object sender, EventArgs e)
+        {
+            if (p.X <= tmargin)
+            {
+                if (r.Y <= p.Y + 25 && r.Y >= p.Y - 25)
+                {
+                    bottomleft.Enabled = false;
+                    bottomright.Enabled = true;
+                }
+                else
+                {
+                    cnt++;
+                }                                
+            }
+            else if (p.Y + bmargin >= this.Size.Height)
+            {
+                bottomleft.Enabled = false;
+                topleft.Enabled = true;
+            }
+            if (cnt <= 10)
+            {
+                r.Y = MousePosition.Y - this.Location.Y - 70;
+                pictureBox3.Location = r;             
+                p.X -= speed;
+                p.Y += speed;
+                pictureBox1.Location = p;
+                q.Y = p.Y;
+                pictureBox2.Location = q;
+            }
+            else
+            {                
+                bottomleft.Enabled = false;
+                levelcounter.Enabled = false;
+                FileStream fs = new FileStream("highscores.txt", FileMode.Append, FileAccess.Write, FileShare.Write);
+                StreamWriter sw = new StreamWriter(fs);                
+                sw.WriteLine("Level: "+level);
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("Game Over");
+                fileToolStripMenuItem.Enabled = true;
+            }
+        }
+        
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fileToolStripMenuItem.Enabled = false;
+            cnt = 0;
+            speed = 5;
+            level = 1;            
+            bottomright.Enabled = true;
+            levelcounter.Enabled = true;
+        }
+        
+        private void levelcounter_Tick(object sender, EventArgs e)
+        {            
+            bottomleft.Enabled = false;
+            topleft.Enabled = false;
+            topright.Enabled = false;
+            bottomright.Enabled = false;
+            levelcounter.Enabled = false;
+            MessageBox.Show("You completed level " + level);            
+            cnt = 0;
+            speed += 2;
+            level++;
+            bottomright.Enabled = true;
+            levelcounter.Enabled = true;
+        }
+
+        private void highscoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("highscores.txt", FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+            StreamReader sr = new StreamReader(fs);
+            string str = sr.ReadToEnd();
+            if (str != "")
+                MessageBox.Show("Scores:\n" + str);
+            else
+                MessageBox.Show("No Highscores Yet");
+            sr.Close();
+            fs.Close();
+        }              
+    }
+}
